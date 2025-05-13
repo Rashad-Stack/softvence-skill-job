e8296c09d96987b58acab58bd798ad47e29ceddf
Niamulhuda-Nahid <niamulhuda6046@gmail.com>
2025-05-14 00:22:02 +0600

    add navbar

--- shared/Navbar.jsx
+++ shared/Navbar.jsx
@@ -1,3 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from '@/assets/images/logo.png';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full shadow-md bg-white fixed top-0 z-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between gap-4">
+        
+        {/* Logo */}
+        <Link href="/" aria-label="Go to homepage">
+          <Image
+            src={logo}
+            alt="Company Logo"
+            width={250}
+            height={40}
+            className="object-contain"
+            priority
+          />
+        </Link>
+
+        {/* Button */}
+        <Link className="text-sm sm:text-base bg-[#038317] text-white px-4 py-2 rounded-lg hover:bg-[#40854a] transition font-pop font-semibold" href="/careers" aria-label="View all job roles">
+            View all roles
+        </Link>
+
+      </div>
+    </nav>
+  )
 }
+
+
